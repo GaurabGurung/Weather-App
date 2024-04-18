34242d477d02235034ebd2fc830af8b80e8f2b78
Gaurab Gurung <gaurabgurung181@gmail.com>
2024-04-18 22:14:07 +1000

    some changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
 import WeatherApp from "./Components/WeatherApp/WeatherApp";
-
+import daySky from "../src/Assets/daySky.avif";
 function App() {
+  const backgroundImageStyle = {
+    backgroundImage: `url(${daySky})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    minHeight: "100vh", // Set the minimum height to cover the entire viewport
+    backdropFilter: "blur(10px)",
+  };
   return (
     <div className="App">
       <WeatherApp />
